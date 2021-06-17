af30bd7d4801dd5ec4d6fbc5fd6345271b88cb97
Teserty <zolman99@gmail.com>
2021-06-17 14:16:51 +0300

    Fix

--- src/main/java/Task1.java
+++ src/main/java/Task1.java
@@ -4,7 +4,7 @@ public class Task1 {
     public void runNotepad(){
         try {
             Process process = new ProcessBuilder("C:\\Program Files (x86)\\Notepad++\\notepad++.exe","--help").start();
-            Thread.sleep(50000);
+            process.waitFor();
             process.destroy();
         } catch (Exception e) {
             e.printStackTrace();
