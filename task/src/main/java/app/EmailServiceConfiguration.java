f731ee7c1402f5b7b326c993da5b8b8d3cde7662
Наиль Шаряпов <zolman99@gmail.com>
2021-06-24 16:13:48 +0300

    warn

--- task/src/main/java/app/EmailServiceConfiguration.java
+++ task/src/main/java/app/EmailServiceConfiguration.java
@@ -9,11 +9,6 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class EmailServiceConfiguration {
 
-    private final SimpleBean simpleBean;
-    @Autowired
-    public EmailServiceConfiguration(SimpleBean simpleBean) {
-        this.simpleBean = simpleBean;
-    }
     @Bean
     public SimpleBean getSimpleBean(){
         return new SimpleBean();
--- task/src/main/java/app/WebApplication.java
+++ task/src/main/java/app/WebApplication.java
@@ -2,16 +2,12 @@ package app;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.context.annotation.Bean;
 import org.springframework.web.WebApplicationInitializer;
 import org.springframework.web.context.ContextLoaderListener;
 import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
-import org.springframework.web.context.support.GenericWebApplicationContext;
-import org.springframework.web.servlet.DispatcherServlet;
 
 import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
-import javax.servlet.ServletRegistration;
 
 
 public class WebApplication implements WebApplicationInitializer {
@@ -23,15 +19,7 @@ public class WebApplication implements WebApplicationInitializer {
         context.register(EmailServiceConfiguration.class);
         context.refresh();
         context.setServletContext(servletContext);
-
         servletContext.addListener(new ContextLoaderListener(context));
-
-        DispatcherServlet dv =
-                new DispatcherServlet(new GenericWebApplicationContext());
-
-        ServletRegistration.Dynamic appServlet = servletContext.addServlet("test-mvc", dv);
-        appServlet.setLoadOnStartup(1);
-        appServlet.addMapping("/test/*");
     }
 
 }
